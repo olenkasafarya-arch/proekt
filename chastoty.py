8eab9c762de52d8de475ce82cf91217d34f8bc1c
olenkasafarya-arch <safari_2006@mail.ru>
2025-11-19 20:19:40 +0300

    Update chastoty.py

--- chastoty.py
+++ chastoty.py
@@ -2,6 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 
+'''MIT License
+
+Copyright (c) 2025 Olga Aleksandrovna Safaryan (Don State Technical University)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. '''
 
 class SignalProcessor:
     def __init__(self):
@@ -1111,4 +1132,5 @@ if __name__ == "__main__":
 
     # 11. Корреляционный критерий
     correlation_results = processor.correlation_criterion(ordered_sequences, observed_signal_sorted, M_omega_vyb,
+
                                                           sigma_vyb)
